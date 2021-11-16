6e55cb6bdcbdd14ff5243a42358f50e57ad4cd76
Matheus Trevizan <matheustreviz@gmail.com>
2021-11-16 20:02:31 +0000

    Merge branch 'main' of github.com:matreviz/treinamento-devops into main

--- 05.1-terraform_ansible_nginx_java-com-mysql/deploy.sh
+++ 05.1-terraform_ansible_nginx_java-com-mysql/deploy.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+cd ~/treinamento-devops/05.1-terraform_ansible_nginx_java-com-mysql
 
 cd terraform
-~/terraform/terraform init
-~/terraform/terraform apply -auto-approve
+terraform init
+terraform apply -auto-approve
 
 echo  "Aguardando a criação das maquinas ..."
 sleep 5
 
-HOST_DNS=$(~/terraform/terraform output | grep 'PUBLIC_DNS=' | awk '{print $1;exit}' | cut -b 13- | sed "s/\",//g")
+HOST_DNS=$(cd ~/treinamento-devops/05.1-terraform_ansible_nginx_java-com-mysql/terraform/ & terraform output | grep 'PUBLIC_DNS=' | awk '{print $1;exit}' | cut -b 13- | sed "s/\",//g")
 
 echo "
 [ec2-java]
@@ -16,13 +17,14 @@ $HOST_DNS
 
 cd ../ansible
 
-USER=root PASSWORD=root DATABASE=SpringWebYoutube ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key ~/Desktop/devops/treinamentoItau
+ANSIBLE_HOST_KEY_CHECKING=False
+USER=root PASSWORD=root DATABASE=SpringWebYoutube ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key ~/.ssh/id_teste
 
-echo  "Abrindo site no navegador"
-sleep 10
+# echo  "Abrindo site no navegador"
+# sleep 10
 
-open "http://$HOST_DNS"
+# open "http://$HOST_DNS"
 
-echo  "Acessando via SH"
-sleep 5
-ssh -i ~/Desktop/devops/treinamentoItau ubuntu@$HOST_DNS -o ServerAliveInterval=60
+# echo  "Acessando via SH"
+# sleep 5
+# ssh -i ~/Desktop/devops/treinamentoItau ubuntu@$HOST_DNS -o ServerAliveInterval=60
