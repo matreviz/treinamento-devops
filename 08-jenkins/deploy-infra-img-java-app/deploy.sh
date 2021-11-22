70d716715371e343712738cb836a397ea2781b52
Matheus Trevizan <matheustreviz@gmail.com>
2021-11-22 18:04:36 +0000

    teste 2

--- 08-jenkins/deploy-infra-img-java-app/deploy.sh
+++ 08-jenkins/deploy-infra-img-java-app/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ~/08-jenkins/deploy-infra-img-java-app/terraform
+cd ~TestePipelineMatheus/08-jenkins/deploy-infra-img-java-app/terraform
 terraform init
 terraform fmt
 terraform apply -auto-approve
@@ -9,12 +9,12 @@ echo "Aguardando criação de maquinas ..."
 sleep 10 # 10 segundos
 
 echo $"[ec2-dev-img-jenkins]" > ../ansible/hosts # cria arquivo
-echo "$(cd ~/08-jenkins/deploy-infra-img-java-app/terraform & terraform output | grep public_dns | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../ansible/hosts # captura output faz split de espaco e replace de ",
+echo "$(cd ~TestePipelineMatheus/08-jenkins/deploy-infra-img-java-app/terraform & terraform output | grep public_dns | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../ansible/hosts # captura output faz split de espaco e replace de ",
 
 echo "Aguardando criação de maquinas ..."
 sleep 10 # 10 segundos
 
 cd ../ansible
 
-echo "Executando ansible ::::: [ ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key /var/lib/jenkins/.ssh/id_rsa ]"
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key ~/.ssh/id_teste
+echo "Executando ansible ::::: [ ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key /var/lib/jenkins/.ssh/id_teste ]"
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key /var/lib/jenkins/.ssh/id_teste
